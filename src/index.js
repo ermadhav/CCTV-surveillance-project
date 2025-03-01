23593c9619655c358432c994994611720e0c4290
ermadhav <madhavtiwari2024@gmail.com>
2025-03-01 14:38:30 +0530

    Updated somthing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
-import './index.css'; // Import your global styles
-import './styles.css'; // Import your custom styles
+import ReactDOM from 'react-dom/client'; 
+import './index.css'; 
+import './styles.css'; 
 import App from './App';
 
-// Create a root and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
